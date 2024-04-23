35d52760f8ff4a67e932259051dea9dd7bf63278
Vedant2311 <skved004@gmail.com>
2024-04-22 17:45:28 -0700

    Rectified the sequence alignment calculation to work for the given test files

--- solutions/basic_3.py
+++ solutions/basic_3.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 def generate_string(base_string, indices):
     current_string = base_string
@@ -37,17 +38,17 @@ def read_and_generate_strings(filename):
 def sequence_alignment(str_1, str_2, gap_penalty, mismatch_cost):
     m = len(str_1)
     n = len(str_2)
-    DP = [[0] * m] * n
-    for i in range(m):
-        DP[0][i] = i*gap_penalty
-    for j in range(n):
-        DP[j][0] = j*gap_penalty
-    for i in range(1, m):
-        for j in range(1, n):
-            DP[i][j] = min( mismatch_cost[str_1[i]][str_2[j]] + DP[i-1][j-1],
-                           gap_penalty + DP[i-1][j],
-                           gap_penalty + DP[i][j-1])
-    return DP[m-1][n-1]
+    DP = np.zeros((m+1,n+1))
+    for i in range(m+1):
+        DP[i,0] = i*gap_penalty
+    for j in range(n+1):
+        DP[0,j] = j*gap_penalty
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            DP[i,j] = min(mismatch_cost[str_1[i-1]][str_2[j-1]] + DP[i-1,j-1],
+                           gap_penalty + DP[i-1,j],
+                           gap_penalty + DP[i,j-1])
+    return DP[m,n]
 
 if __name__ == "__main__":
     input_file = sys.argv[1]
@@ -56,9 +57,9 @@ if __name__ == "__main__":
     str_1, str_2 = read_and_generate_strings(input_file)
     
     # print strings for testing (delete before submission)
-    if str_1 and str_2:
-        print("Generated String 1:", str_1)
-        print("Generated String 2:", str_2)
+    # if str_1 and str_2:
+    #     print("Generated String 1:", str_1)
+    #     print("Generated String 2:", str_2)
 
     gap_penalty = 30
     mismatch_cost = {
