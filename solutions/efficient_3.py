142492e1a84e5093a3562f902619980b0fadc572
rmarrcode <r.marr747@gmail.com>
2024-05-03 16:47:14 -0700

    mem effict working i think

--- solutions/efficient_3.py
+++ solutions/efficient_3.py
@@ -1,1 +1,194 @@
-# memory efficient algorithm
+import sys
+import time
+import psutil
+import numpy as np
+
+def generate_string(base_string, indices):
+    current_string = base_string
+    
+    for index in indices:
+        if index < len(current_string):
+            current_string = current_string[:index + 1] + current_string + current_string[index + 1:]
+        else:
+            current_string += current_string
+    return current_string
+
+def read_and_generate_strings(filename):
+    with open(filename, 'r') as file:
+        lines = file.read().splitlines()
+        
+        strings = []
+        indices = []
+        current_base_string = lines[0]
+
+        for line in lines[1:]:
+            try:
+                index = int(line)
+                indices.append(index)
+            except ValueError:
+                generated_string = generate_string(current_base_string, indices)
+                strings.append(generated_string)
+                
+                indices = []
+                current_base_string = line
+        
+        generated_string = generate_string(current_base_string, indices)
+        strings.append(generated_string)
+
+        return strings
+
+def top_down_pass(DP, str_1, str_2):
+    aligned_str_1 = ""
+    aligned_str_2 = ""
+    m = len(str_1)
+    n = len(str_2)
+    i, j = m, n
+
+    # Go through the constructed DP table to get the accurate string alignments
+    while i!=0 and j!=0:
+        mismatch = mismatch_cost[str_1[i-1]][str_2[j-1]] + DP[i-1,j-1]
+        skip_str_1 = gap_penalty + DP[i-1,j]
+        skip_str_2 = gap_penalty + DP[i,j-1]
+        min_index = np.argmin(np.array([mismatch, skip_str_1, skip_str_2]))
+
+        if min_index == 0:
+            aligned_str_1 += str_1[i-1]
+            aligned_str_2 += str_2[j-1]
+            i = i-1
+            j = j-1
+        elif min_index == 1:
+            aligned_str_1 += str_1[i-1]
+            aligned_str_2 += "_"
+            i = i-1
+        else:
+            aligned_str_1 += "_"
+            aligned_str_2 += str_2[j-1]
+            j = j-1
+
+    # We need to align the strings with "_" once only one string has been fully consumed
+    while i > 0:
+        aligned_str_1 += str_1[i-1]
+        aligned_str_2 += "_"
+        i = i-1
+    while j > 0:
+        aligned_str_1 += "_"
+        aligned_str_2 += str_2[j-1]
+        j = j-1
+
+    # Reverse the strings and return them
+    return aligned_str_1[::-1], aligned_str_2[::-1]
+
+def sequence_alignment_basic(str_1, str_2, gap_penalty, mismatch_cost):
+    m = len(str_1)
+    n = len(str_2)
+    DP = np.zeros((m+1,n+1))
+    for i in range(m+1):
+        DP[i,0] = i*gap_penalty
+    for j in range(n+1):
+        DP[0,j] = j*gap_penalty
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            DP[i,j] = min(mismatch_cost[str_1[i-1]][str_2[j-1]] + DP[i-1,j-1],
+                           gap_penalty + DP[i-1,j],
+                           gap_penalty + DP[i,j-1])
+    return DP, DP[m,n]
+
+def build_table(X, Y, gap_penalty, mismatch_cost):
+
+    m = len(X)
+    n = len(Y)
+    DP_old = np.zeros(n+1)
+    DP_cur = np.zeros(n+1)
+
+    for i in range(1, n+1):
+        DP_old[i] = i*gap_penalty
+    
+    for i in range(1, m+1):
+        DP_cur[0] = gap_penalty*i
+        for j in range(1, n+1):
+            DP_cur[j] = min(mismatch_cost[X[i-1]][Y[j-1]] + DP_old[j-1],
+                            gap_penalty + DP_old[j],
+                            gap_penalty + DP_cur[j-1])
+            
+        DP_old=np.copy(DP_cur)
+            
+    return DP_cur
+
+def divide_and_conquer(X, Y, gap_penalty, mismatch_cost):
+    m = len(X)
+    n = len(Y)
+    if m < 2 or n < 2:
+        DP, cost = sequence_alignment_basic(X, Y, gap_penalty, mismatch_cost)
+        X_edit, Y_edit = top_down_pass(DP, X, Y)
+        return cost, X_edit, Y_edit
+
+    cost_xl = build_table(X[:m//2], Y, gap_penalty, mismatch_cost)
+    cost_xr = build_table(X[m//2:][::-1], Y[::-1], gap_penalty, mismatch_cost)
+
+    places_to_cut = [cost_xl[j] + cost_xr[n - j] for j in range(n + 1)]
+    min_cost = min(places_to_cut)
+    c = places_to_cut.index(min_cost)
+
+    cost_l, x_l, y_l = divide_and_conquer(X[:m//2], Y[:c], gap_penalty, mismatch_cost)
+    cost_r, x_r, y_r = divide_and_conquer(X[m//2:], Y[c:], gap_penalty, mismatch_cost)
+
+    return (cost_l+cost_r, x_l+x_r, y_l+y_r)
+
+def get_alignment_cost(aligned_str_1, aligned_str_2, gap_penalty, mismatch_cost):
+    # This function will compute the alignment cost between the given aligned strings
+    m = len(aligned_str_1)
+    if len(aligned_str_2) != m:
+        return -1
+    cost = 0.
+    for i in range(m):
+        if aligned_str_1[i] == "_" or aligned_str_2[i] == "_":
+            cost += gap_penalty
+        else:
+            cost += mismatch_cost[aligned_str_1[i]][aligned_str_2[i]]
+    return cost
+
+def process_memory():
+    process = psutil.Process()
+    memory_info = process.memory_info()
+    memory_consumed = int(memory_info.rss / 1024)  # memory in KB
+    return memory_consumed
+
+def time_wrapper(function, *args, **kwargs):
+    start_time = time.time()
+    result = function(*args, **kwargs)
+    end_time = time.time()
+    time_taken = (end_time - start_time) * 1000  # time in milliseconds
+    return result, time_taken
+
+if __name__ == "__main__":
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+    
+    str_1, str_2 = read_and_generate_strings(input_file_path)
+
+    gap_penalty = 30
+    mismatch_cost = {
+        'A': {'A': 0, 'C': 110, 'G': 48, 'T': 94},
+        'C': {'A': 110, 'C': 0, 'G': 118, 'T': 48},
+        'G': {'A': 48, 'C': 118, 'G': 0, 'T': 110},
+        'T': {'A': 94, 'C': 48, 'G': 110, 'T': 0}
+    }
+    
+    memory_before = process_memory()
+    (cost, aligned_str_1, aligned_str_2), time_taken = time_wrapper(divide_and_conquer, str_1, str_2, gap_penalty, mismatch_cost)
+
+    memory_after = process_memory()
+    memory_used = memory_after - memory_before
+
+    print(cost)
+    print(aligned_str_1)
+    print(aligned_str_2)
+    print(time_taken)
+    print(memory_used)
+    
+    # with open(output_file_path, 'w') as file:
+    #     file.write(f"{int(cost)}\n")
+    #     file.write(f"{aligned_str_1}\n")
+    #     file.write(f"{aligned_str_2}\n")
+    #     file.write(f"{time_taken}\n")
+    #     file.write(f"{memory_used}")
