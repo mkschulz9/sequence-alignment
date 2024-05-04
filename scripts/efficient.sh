4179d8c354afc476c82531818fefdd5d12b8eadb
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2024-05-03 19:10:59 -0700

    build: update scripts

--- scripts/efficient.sh
+++ scripts/efficient.sh
@@ -14,10 +14,22 @@ fi
 # activate the virtual env
 source $VENV_PATH/Scripts/activate
 
-# upgrade pip and install required packages
-echo "Upgrading pip and installing required packages..."
-pip install --upgrade pip
-pip install numpy psutil
+# Function to check and install a package if not already installed
+function check_and_install {
+    PACKAGE=$1
+    if ! pip list | grep -F $PACKAGE > /dev/null; then
+        echo "Installing $PACKAGE..."
+        pip install $PACKAGE
+    else
+        echo "$PACKAGE is already installed."
+    fi
+}
+
+# Check and install necessary packages
+check_and_install "numpy"
+check_and_install "psutil"
 
 # run the basic program
 python ./solutions/efficient_3.py $INPUT_FILE $OUTPUT_FILE
+
+deactivate
